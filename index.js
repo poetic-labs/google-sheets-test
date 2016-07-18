44eb0b317acca6bc44100b1182cf95f6140e6d73
martinbee <martin@poeticsystems.com>
2016-07-18 10:47:01 -0500

    add batch example.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var GoogleSpreadsheet = require('google-spreadsheet');
 
 // uses google sheets id; see google sheets docs.
-var doc = new GoogleSpreadsheet(// sheetId);
+var doc = new GoogleSpreadsheet('1GtgJwQj8Z5TfCjtJEbU2ZfMlcXiTzvr_TMYgfyeJ898');
 
 doc.getInfo(function(err, info) {
   if (err) {
@@ -10,12 +10,28 @@ doc.getInfo(function(err, info) {
 
   console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
   sheet = info.worksheets[0];
-  console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+  console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + ' rows long, ' + sheet.colCount + ' columns.');
 
-  // make separate call to sheet and check rows
-  // offest is the row the search begins at, limit is how many rows to check
-  sheet.getRows({ offset: 1, limit: 10 }, function( err, rows ){
-    console.log('Read '+ rows.length +' rows');
-    console.log(rows);
+  var batches = Array.from(Array(Number(Math.ceil(sheet.rowCount/1000))).keys());
+
+  var getOffset = 1;
+  var getLimit = 1000
+
+  batches.forEach(function(batch, index) {
+    if (index === batches.length -1) {
+      var remainder = sheet.rowCount % 1000;
+      if (remainder === 0) {
+        getLimit = 1000;
+      } else {
+        getLimit = remainder;
+      }
+    }
+
+    sheet.getRows({ offset: getOffset, limit: getLimit }, function( err, rows ){
+      console.log('Read '+ rows.length +' rows, batch #' + index);
+    });
+
+    getOffset += 1000;
   });
+
 });
